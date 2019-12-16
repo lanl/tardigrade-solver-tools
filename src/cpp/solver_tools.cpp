2ee63a8524d3aead932f26b49d531f33b2d8bc73
Nathan Andrew Miller <nathanm@lanl.gov>
2019-12-16 08:42:03 -0700

    Added solver tools files

---
+++ src/cpp/solver_tools.cpp
@@ -0,0 +1,18 @@
+/*!
+===============================================================================
+|                             solver_tools.cpp                                |
+===============================================================================
+| The solver tools library. Incorporates a collection of non-linear solver    |
+| tools built on top of Eigen. These tools are intended to be general enough  |
+| to solve any small-ish nonlinear problem that can be solved using a Newton  |
+| Raphson approach.                                                           |
+===============================================================================
+*/
+
+#include<solver_tools.h>
+
+namespace solverTools{
+
+}
+
+#endif
---
+++ src/cpp/solver_tools.h
@@ -0,0 +1,25 @@
+/*!
+===============================================================================
+|                               solver_tools.h                                |
+===============================================================================
+| The solver tools library. Incorporates a collection of non-linear solver    |
+| tools built on top of Eigen. These tools are intended to be general enough  |
+| to solve any small-ish nonlinear problem that can be solved using a Newton  |
+| Raphson approach.                                                           |
+===============================================================================
+*/
+
+#ifndef SOLVER_TOOLS_H
+#define SOLVER_TOOLS_H
+
+#define USE_EIGEN
+#include<vector_tools.h>
+#include<error_tools.h>
+
+namespace solverTools{
+
+    
+
+}
+
+#endif
